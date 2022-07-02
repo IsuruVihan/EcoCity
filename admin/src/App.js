d1336e815516d34deceefc0834adf2eb186ddd57
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-02 10:37:53 +0530

    Folder structure created

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import packages
+import { Container } from "react-bootstrap";
+// import components
+import Comp1 from "./components/Comp1";
+// import styles
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      Admin Dashboard
+      <Comp1 />
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ admin/src/components/Comp1.js
@@ -0,0 +1,15 @@
+// import packages
+import { Container } from "react-bootstrap";
+// import components
+// import styles
+import "../assets/styles/Comp1.css";
+
+const Comp1 = () => {
+  return (
+    <Container className="hello">
+      Test component
+    </Container>
+  );
+}
+
+export default Comp1;
