9fe4f2e516417df5ffcf243e4ad5e9ad02102c67
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-18 19:23:37 +0530

    LoadingScreen1.js, LoadingScreen2.js, and Login.js sketch files added

---
+++ citizen/screens/LoadingScreen2.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const LoadingScreen2 = () => {
+  return (
+    <View>
+      <Text>Loading screen 2</Text>
+    </View>
+  );
+}
+
+export default LoadingScreen2;
