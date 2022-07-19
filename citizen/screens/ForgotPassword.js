d9aa5e52575c1658def7350d5e4d1292b3f406b1
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-19 23:14:40 +0530

    Navigation between Login view and ForgotPassword view done

--- citizen/screens/ForgotPassword.js
+++ citizen/screens/ForgotPassword.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from "react-native";
 
-const ForgotPassword = () => {
+const ForgotPassword = ({navigation}) => {
   return (
     <View>
       <Text>Forgot password</Text>
+      <Text onPress={() => navigation.goBack()}>Back to login</Text>
     </View>
   );
 }
