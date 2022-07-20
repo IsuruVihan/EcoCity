963f7c197c5e6851ca1877b15039df2b58e6f68f
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-20 11:35:34 +0530

    added logout icon

--- admin/src/common/Header.js
+++ admin/src/common/Header.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {Col, Container, Navbar, Row} from "react-bootstrap";
+import {Col, Container, Image, Navbar, Row} from "react-bootstrap";
 import {Link, useLocation} from "react-router-dom";
 import '../assets/styles/common/Header.css';
 import logo from '../assets/images/Logo1.png';
+import logoutIcon from '../assets/images/icons/log-out.png';
 
 const Header = () => {
     const location = useLocation();
@@ -18,27 +19,27 @@ const Header = () => {
         //Logged In header
         return (
             <Container className='py-2 mx-0 bg-transparent position-absolute top-0' fluid>
-                <Row className='border'>
-                    <Navbar className='navigation-bar mx-3 fs-5 mt-0 pt-1'>
-                        <Col lg={2}>
-                            <Navbar.Brand href="#home">
-                                <img
-                                    src={logo}
-                                    height="110"
-                                    className="d-inline-block align-top"
-                                    alt="React Bootstrap logo"
-                                />
-                            </Navbar.Brand>
 
-                        </Col>
-                        <Col lg={7} className='justify-content-end d-flex pe-lg-5'>
-                            gjhsadgjh
-                        </Col>
-                        <Col lg={3} className='vertical-divider ps-lg-5'>
-                            sdajksh
-                        </Col>
-                    </Navbar>
-                </Row>
+                <Navbar className='navigation-bar mx-3 fs-5 mt-0 pt-1'>
+                    <Col lg={2}>
+                        <Navbar.Brand href="#home">
+                            <img
+                                src={logo}
+                                height="110"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
+                            />
+                        </Navbar.Brand>
+
+                    </Col>
+                    <Col lg={7} className='justify-content-end d-flex pe-lg-5'>
+
+                    </Col>
+                    <Col lg={3} className='vertical-divider ps-lg-5  d-flex align-items-center justify-content-end'>
+                        <Image src={logoutIcon} className='logout-icon' fluid/>
+                    </Col>
+                </Navbar>
+
             </Container>
         )
     }
@@ -46,11 +47,14 @@ const Header = () => {
     //Logged out header
     return (
         <Container className='py-2 mx-0 bg-transparent position-absolute top-0' fluid>
-            <Navbar className='navigation-bar justify-content-end mx-5 fs-5 mt-2'>
-                <Link to='/' className={'me-3 px-4 py-2  ' + (url === '/' ? 'active' : '')}>Home</Link>
-                <Link to='/about-us' className={'px-4 py-2 ' + (url === '/about-us' ? 'active' : '')}>About Us</Link>
-                <Link to='/login' className={'px-4 py-2 ' + (url === '/login' ? 'active' : '')}>Login</Link>
-            </Navbar>
+            <Row>
+                <Navbar className='navigation-bar justify-content-end mx-5 fs-5 mt-2'>
+                    <Link to='/' className={'me-3 px-4 py-2  ' + (url === '/' ? 'active' : '')}>Home</Link>
+                    <Link to='/about-us' className={'px-4 py-2 ' + (url === '/about-us' ? 'active' : '')}>About
+                        Us</Link>
+                    <Link to='/login' className={'px-4 py-2 ' + (url === '/login' ? 'active' : '')}>Login</Link>
+                </Navbar>
+            </Row>
         </Container>
 
     );
