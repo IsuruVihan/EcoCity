a47bdcfbc9382e2fdb438745058176967971e747
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-20 23:48:55 +0530

    Completed the footer UI

--- admin/src/common/Footer.js
+++ admin/src/common/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
+import '../assets/styles/common/Footer.css';
 
 const Footer = () => {
-  return (
-    <Container>
-      Footer
-    </Container>
-  );
+    return (
+        <Row className='w-100 px-4  py-2 position-absolute bottom-0 justify-content-center bor'>
+            <div className='footer-line w-100 mb-3'/>
+            &copy; EcoCity {(new Date().getFullYear())}
+        </Row>
+    );
 }
 
 export default Footer;
