d8c65b23c5dd6dd8935cead9ddd0a5bb8efb5dd2
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-20 11:05:21 +0530

    Did some alignment changes, header component added, resized image

--- admin/src/pages/Login/Login.js
+++ admin/src/pages/Login/Login.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import sideImage from '../../assets/images/login-side-image.png';
+import {Container, Row, Col, Image} from 'react-bootstrap';
+
 import LoginForm from "./forms/LoginForm";
 
+import sideImage from '../../assets/images/login-side-image.png';
+import Header from "../../common/Header";
 
 const Login = () => {
   return (
-    <Container className="mx-0 " fluid  >
-        <Row>
-            <Col className="border border-3 border-primary">
-
-                <Image src={sideImage} fluid/>
-            </Col>
-            <Col className="border border-3 border-danger">
-                form
-                {/*<LoginForm/>*/}
-            </Col>
-        </Row>
-
+    <Container className="mx-0 px-0" fluid>
+      <Row className="mx-0 border border-1 border-danger">
+        <Header/>
+      </Row>
+      <Row className="mx-0">
+        <Col className="px-0 border border-1 border-primary">
+          <Image src={sideImage} width={'80%'}/>
+        </Col>
+        <Col className="px-0 border border-1 border-danger">
+          form
+          {/*<LoginForm/>*/}
+        </Col>
+      </Row>
     </Container>
-
   );
 }
 
