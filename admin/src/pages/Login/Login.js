413d21c1e7c6390a2583920801bff8dfa8c0605b
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-20 12:34:06 +0530

    Removed the temporary header component added previously

--- admin/src/pages/Login/Login.js
+++ admin/src/pages/Login/Login.js
@@ -2,15 +2,13 @@ import React from 'react';
 import {Container} from "react-bootstrap";
 
 import LoginForm from "./forms/LoginForm";
-import Header from "../../common/Header";
 
 const Login = () => {
-  return (
-    <Container>
-        <Header />
-      Login
-    </Container>
-  );
+    return (
+        <Container>
+            Login
+        </Container>
+    );
 }
 
 export default Login;
