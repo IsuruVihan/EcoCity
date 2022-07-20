f4064916ace1644f1fdc9258bc2263ea0c2c0426
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-07-20 08:20:23 +0530

    Login page | complete sketch done

--- admin/src/pages/Login/Login.js
+++ admin/src/pages/Login/Login.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
-
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import sideImage from '../../assets/images/login-side-image.png';
 import LoginForm from "./forms/LoginForm";
+
 
 const Login = () => {
   return (
-    <Container>
-      Login
+    <Container className="mx-0 " fluid  >
+        <Row>
+            <Col className="border border-3 border-primary">
+
+                <Image src={sideImage} fluid/>
+            </Col>
+            <Col className="border border-3 border-danger">
+                form
+                {/*<LoginForm/>*/}
+            </Col>
+        </Row>
+
     </Container>
+
   );
 }
 
