cbc9679b3221e3230194a2f1b2c3227225e6703e
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-20 19:47:49 +0530

    Added notification model popup on click

---
+++ admin/src/common/Notifications/Notifications.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+import '../../assets/styles/common/Notifications.css';
+
+const Notifications = () => {
+    return (
+        <Container className='border-red notification-container position-absolute h-50'>
+            notifications
+        </Container>
+    );
+}
+
+export default Notifications;
