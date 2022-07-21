dc00544b56fa16941a5711a728f479d688e6913a
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-07-21 20:12:35 +0530

    Renamed the branch Added some space under the text and changed the color

--- admin/src/common/Footer.js
+++ admin/src/common/Footer.js
@@ -4,9 +4,12 @@ import '../assets/styles/common/Footer.css';
 
 const Footer = () => {
     return (
-        <Row className='w-100 px-4  py-2 position-absolute bottom-0 justify-content-center bor'>
+        <Row className='w-100 px-4  py-2 position-absolute bottom-0 justify-content-center '>
             <div className='footer-line w-100 mb-3'/>
-            &copy; EcoCity {(new Date().getFullYear())}
+            <div className='footer-text mb-2'>
+                &copy; EcoCity {(new Date().getFullYear())}
+            </div>
+
         </Row>
     );
 }
