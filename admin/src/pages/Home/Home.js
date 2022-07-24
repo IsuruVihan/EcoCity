4ff815de580a2795befd977839ba36c611f341ad
omaya98 <theruniomayawickramasinghe98@gmail.com>
2022-07-24 17:29:26 +0530

    created home page

--- admin/src/pages/Home/Home.js
+++ admin/src/pages/Home/Home.js
@@ -1,20 +1,50 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container , Row , Col } from "react-bootstrap";
+import './Home.css'
 
 import BinTypeStatus from "./cards/BinTypeStatus";
 import OurMission from "./cards/OurMission";
 import GarbageCollectionBarGraph from "./graphs/GarbageCollectionBarGraph";
 import TeamMemberCard from "./cards/TeamMemberCard";
 import Header from "../../common/Header";
-import Footer from "../../common/Footer";
+import Footer from '../../common/Footer';
+import memberIcon from '../../assets/images/our-group.png'
 
 const Home = () => {
   return (
-    <Container className='d-flex justify-content-around mx-0 px-0' fluid>
-      {/*<Header />*/}
+    <div className='homeHeader'>
+      <Header />
+      <div className='containerHome'>
+        <Container  fluid className='border'>
+          <div className='homePage'></div>
+        
+        </Container>
+        <div className='ourMission border'>
+          <OurMission/>
+        </div>
+        <div>
+          <Row fluid>
+            <Col sm={4} className='binType border'>
+              <BinTypeStatus/>
+            </Col>
 
-        <Footer />
-    </Container>
+            <Col sm={8} className='bargraph border'>
+              <GarbageCollectionBarGraph/>
+            </Col>
+
+          </Row>
+
+        </div>
+        <div className='groupMembers-icon border'></div>
+        <TeamMemberCard/>
+
+        <Footer/>
+
+      </div>
+
+    </div>
+
+  
   );
 }
 
