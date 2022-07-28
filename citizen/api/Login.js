93f46a0a611de4fe68df1cc26f56dd3c7d471f26
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-28 14:22:48 +0530

    Setup permissions in AndroidManifest.xml

--- citizen/api/Login.js
+++ citizen/api/Login.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const url = 'http://192.168.1.227:3001/api';
-
 export const loginUser = async ({email, password}) => {
-  return await axios.post(`http://192.168.103.61:3001/api/login`, {
+  return await axios.post(`http://192.168.1.227:3001/api/login`, {
     type: 'citizen',
     email: email,
     password: password,
@@ -11,7 +9,7 @@ export const loginUser = async ({email, password}) => {
 }
 
 export const getLoggedInUser = async (accessToken, refreshToken) => {
-  return await axios.post(`http://192.168.103.61:3001/api/login/current`, {}, {
+  return await axios.post(`http://192.168.1.227:3001/api/login/current`, {}, {
     headers: {
       accessToken: accessToken,
       refreshToken: refreshToken
