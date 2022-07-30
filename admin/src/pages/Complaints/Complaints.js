d761934c988e110f222bd1b83902d600e9ecb765
Pavi99sandamini151 <pavithrasandamini283@gmail.com>
2022-07-30 08:14:34 +0530

    admin task 318 filter option UI/UX done

--- admin/src/pages/Complaints/Complaints.js
+++ admin/src/pages/Complaints/Complaints.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import React , {useState} from 'react';
+import {Col, Container, Image, Row , Collapse , Button } from "react-bootstrap";
 
 import SideNav from "../../common/SideNav";
 import ComplaintsTableFilter from "./filters/ComplaintsTableFilter";
@@ -7,20 +7,246 @@ import ComplaintsTable from "./tables/ComplaintsTable";
 import ViewComplaintModal from "./modals/ViewComplaintModal";
 import Header from "../../common/Header";
 
+import filtericon from "../../assets/images/icons/filter.png";
+import filtericonbutton from "../../assets/images/icons/filterwhite.png";
+import from_date from "../../assets/images/icons/from-date.png";
+import to_date from "../../assets/images/icons/to-date.png";
+
 const Complaints = () => {
+
+    const [open, setOpen] = useState(false);
   return (
-      <Col className='' lg={10}>
-          Complaints
+      <Col  lg={10} >
+          <Row className="ps-3 pb-3 pt-2">
+              <Col className=" p-0 m-0"
+                   style={{textAlign: 'left',
+                       fontSize: 30,
+                       fontWeight: '900',
+                       color: '#042434' ,}}>
+                  Complaints
+              </Col>
+          </Row>
+          <Row>
+              <Col
+                  style={{border:"1px solid #BFDDDE" , borderRadius:10}} className="m-2">
+                  <Row className="d-flex border ">
+                      <Col xs={10}></Col>
+                      <Col  className='pt-3 px-4 border ' >
+                          <Row xs="2"
+                               style={{border:'2px solid #BFDDDE', borderRadius:10 }} >
+                              <Col
+                                  onClick={() => setOpen(!open)}
+                                  style={{ cursor:'pointer' }}
+                                  aria-controls="example-collapse-text"
+                                  aria-expanded={open}
+                                  xs="6">
+                                  Filter
+                              </Col>
+                              <Col
+                                  style={{ textAlign:'right' ,}}>
+                                  <Image src={filtericon} width="50%" >
+                                  </Image>
+                              </Col>
+                          </Row>
+
+                      </Col>
+                  </Row>
+                  <Row style={{position:'absolute',  right:25 , paddingTop:8}} >
+                      <Collapse in={open}  >
+                          <Container id="example-collapse-text">
+                              <Row className=" " >
+                                  <Col></Col>
+                                  <Col style={{borderBottomRightRadius:15 ,
+                                       borderBottomLeftRadius:15,
+                                       boxShadow: `0px 4px 8px #707070`,}} lg={5}>
+                                      <Row className="m-1">
+                                          <Col>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11, marginLeft:5}}>
+                                                          All
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11 , marginLeft:5}}>
+                                                          Resolved
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11 , marginLeft:5}}>
+                                                          Removed
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                          </Col>
+                                          <Col>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11 , marginLeft:5}}>
+                                                          Viewed
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11 , marginLeft:5}}>
+                                                          In-progress
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <input type='checkbox'
+                                                             style={{cursor: 'pointer'}}/>
+                                                      <label
+                                                          style={{fontSize:11 , marginLeft:5}}>
+                                                          Open
+                                                      </label>
+                                                  </Col>
+                                              </Row>
+                                          </Col>
+                                      </Row>
+                                      <Row style={{borderTop:'2px solid #BFDDDE',
+                                          borderBottom:'2px solid #BFDDDE',
+                                          marginLeft:.5 ,
+                                          marginRight:.5 ,
+                                          paddingBottom:10}} >
+                                          <Col>
+                                              <Row>
+                                                  <Col>
+                                                      <label
+                                                          style={{fontSize:9}}>
+                                                          ComplaintID
+                                                      </label>
+                                                  </Col>
+                                                  <Col xs={8} >
+                                                      <input
+                                                          style={{width:130 ,
+                                                          border:'none' ,
+                                                          outline:'none',
+                                                          borderBottom:'2px solid #E9E8EF'}}/>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <label
+                                                          style={{fontSize:9}}>
+                                                          FirstName
+                                                      </label>
+                                                  </Col>
+                                                  <Col xs={8} >
+                                                      <input
+                                                          style={{width:130 ,
+                                                          border:'none' ,
+                                                          outline:'none',
+                                                          borderBottom:'2px solid #E9E8EF'}}/>
+                                                  </Col>
+                                              </Row>
+                                              <Row>
+                                                  <Col>
+                                                      <label
+                                                          style={{fontSize:9}}>
+                                                          LastName
+                                                      </label>
+                                                  </Col>
+                                                  <Col xs={8} >
+                                                      <input
+                                                          style={{width:130 ,
+                                                          border:'none' ,
+                                                          outline:'none' ,
+                                                          borderBottom:'2px solid #E9E8EF'}}/>
+                                                  </Col>
+                                              </Row>
+                                              <Row className="mx-1 mt-3 ">
+                                                  <Col style={{textAlign:'left'}} xs={5}>
+                                                      <Image src={from_date} width="40%"/>
+                                                  </Col>
+                                                  <Col style={{textAlign:'left',
+                                                       fontSize:12,
+                                                       color:'#BFDDDE',
+                                                       fontWeight:'bold'}}>
+                                                      From date
+                                                      <input type="date"
+                                                             style={{border:'none' ,
+                                                             color:'#228693',
+                                                             outline:'none'}}/>
+                                                  </Col>
+
+                                              </Row>
+                                              <Row className="mx-1 mt-3">
+                                                  <Col style={{textAlign:'left'}} xs={5} >
+                                                      <Image src={to_date} width="40%"/>
+                                                  </Col>
+                                                  <Col style={{textAlign:'left',
+                                                       fontSize:12,
+                                                       fontWeight:'bold',
+                                                       color:'#BFDDDE'}}>
+                                                      To date
+                                                      <input type="date"
+                                                             style={{border:'none' ,
+                                                             color:'#BFDDDE' ,
+                                                             outline:'none'}}/>
+                                                  </Col>
+
+                                              </Row>
+                                          </Col>
+                                      </Row>
+                                      <Row  className="me-1 mt-1 mb-1 p-1"
+                                            style={{ marginLeft:'60%' ,}}>
+                                          <Col>
+                                              <Row style={{backgroundColor:'#228693' ,
+                                                           borderRadius:10 ,
+                                                           paddingBottom:5 ,
+                                                           paddingTop:5}}>
+                                                  <Col style={{textAlign:'right' ,}} xs={4}>
+                                                      <Image src={filtericonbutton}  width="250%"/>
+                                                  </Col>
+                                                  <Col style={{ color:'#fff' ,
+                                                                textAlign:'left' ,
+                                                                fontSize:15 ,
+                                                                fontWeight:'bold',
+                                                                paddingTop:4,
+                                                                cursor:'pointer'}}>
+                                                      Filter
+                                                  </Col>
+                                              </Row>
+                                          </Col>
+                                      </Row>
+                                  </Col>
+                              </Row>
+                          </Container>
+                      </Collapse>
+                  </Row>
+                  {/*<Row >*/}
+                  {/*    <Col*/}
+                  {/*        style={{border:"1px solid #BFDDDE" , }} className="m-4">*/}
+                  {/*        table*/}
+                  {/*    </Col>*/}
+                  {/*</Row>*/}
+
+              </Col>
+          </Row>
       </Col>
-      // <Container className='mx-0 px-0 h-100 overflow-hidden' fluid>
-      //     <Header/>
-      //     <Row className='mx-0 mt-72 container-section' >
-      //         <SideNav/>
-      //         <Col className='' lg={10}>
-      //             Complaints
-      //         </Col>
-      //     </Row>
-      // </Container>
+
   );
 }
 
