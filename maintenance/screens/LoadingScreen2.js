9f9e1158a793690e9df2d64edde2d25b75dce9dd
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-30 21:25:01 +0530

    driver_task_139 to driver_task_149, driver_task_182, driver_task_186 done

---
+++ maintenance/screens/LoadingScreen2.js
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react';
+import {View, Image, StatusBar} from 'react-native';
+import {StackActions} from '@react-navigation/native';
+
+import LoadingScreen2Img from '../assets/images/LoadingScreen2.png';
+
+const LoadingScreen2 = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(
+      () => navigation.dispatch(StackActions.replace('Login')),
+      1000
+    );
+  }, []);
+
+  return (
+    <View style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <StatusBar hidden={false} backgroundColor={'#228693'}/>
+      <Image source={LoadingScreen2Img} style={{width: '100%', height: '100%'}} />
+    </View>
+  );
+}
+
+export default LoadingScreen2;
