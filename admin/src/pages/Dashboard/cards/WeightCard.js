5c7d36d5b8b7324f432e2fa0b0258bd353b44c35
IndunJD <indunijd@gmail.com>
2022-08-01 20:36:00 +0530

    admin_task_227

--- admin/src/pages/Dashboard/cards/WeightCard.js
+++ admin/src/pages/Dashboard/cards/WeightCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row, Col, Image} from "react-bootstrap";
 
 const WeightCard = () => {
   return (
-    <Container>
-      Weight card
+    <Container className="p-0">
+      <Row className="m-0" style={{border: '2px solid green'}}>
+          <Col className="p-0" sm="12"></Col>
+          <Col className="p-0" sm="12"></Col>
+      </Row>
     </Container>
   );
 }
