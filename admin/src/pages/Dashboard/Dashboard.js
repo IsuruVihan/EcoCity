61a14f3696990d17c273302420f9ca8d52703a72
IndunJD <indunijd@gmail.com>
2022-08-02 19:17:45 +0530

    WeightCards UI/UX done

--- admin/src/pages/Dashboard/Dashboard.js
+++ admin/src/pages/Dashboard/Dashboard.js
@@ -10,12 +10,16 @@ import Header from "../../common/Header";
 import Footer from "../../common/Footer";
 
 const Dashboard = () => {
+
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
     return (
         <Col className='' lg={10}>
             <Row className="m-0">
                 <Col className="p-0 mt-4" style={{
                     fontWeight: 800, fontSize: 30, color: '#075061'}} sm={12}>Hello Nisal,</Col>
-                <Col className="p-0 mb-3" sm={12}>This is what we have got for you today!</Col>
+                <Col className="p-0 mb-3" sm={12}>This is what we have got for you today - {date}</Col>
                 <Col lg={10} className="p-0" style={{border: '2px solid red'}}>
                     <Row className="m-0" style={{border: '2px solid #BFDDDE', borderRadius: '20px'}}>
                         <Col lg={12} className="p-0 mt-2 ms-2" style={{
