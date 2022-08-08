25d925f7a8ae8a3acd332749cdf0297d4198f6fe
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-08 12:32:20 +0530

    Save

--- admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
+++ admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Col, Form, FormCheck, Row} from "react-bootstrap";
+import {Col, Form, FormCheck, FormControl, FormLabel, Row} from "react-bootstrap";
 import {FiFilter} from "react-icons/fi";
+import Select from "react-select";
 
 const GarbageHubsTableFilter = () => {
 
@@ -8,26 +9,38 @@ const GarbageHubsTableFilter = () => {
         console.log('filterrrr');
     }
 
+    const binTypeOptions = [
+        {value: 'chocolate', label: 'Chocolate'},
+        {value: 'strawberry', label: 'Strawberry'},
+        {value: 'vanilla', label: 'Vanilla'}
+    ]
+
     return (
-        <div className='position-absolute garbage-hubs-table-filter-wrapper h-50 me-3'>
-            <Row className='mx-0'>
-                <Col lg={12}>Hub ID</Col>
-                <Col lg={12}>Bin Type</Col>
+        <div className='position-absolute garbage-hubs-table-filter-wrapper me-3'>
+            <Row className='mx-0 filter-inputs my-2'>
+                <Col lg={12} className='d-flex align-items-center'>
+                    <label className='me-1'>Hub ID</label>
+                    <input type='text'/>
+                </Col>
+                <Col lg={12} className='d-flex align-items-center'>
+                    <label className='me-1'>Bin Type</label>
+                    <Select/>
+                </Col>
             </Row>
-            <Row className='mx-0 filters-section'>
-                <Form>
-                    <FormCheck label='All'/>
-                    <FormCheck label='Active'/>
-                    <FormCheck label='Under Maintenance'/>
-                    <FormCheck label='Unavailable'/>
-                    <FormCheck label='Removed'/>
+            <Row className='mx-0 filters-section mt-2 mb-2'>
+                <Form className='ms-2 w-auto'>
+                    <FormCheck id='all' label='All'/>
+                    <FormCheck id='active' label='Active' style={{color: '#00C186'}}/>
+                    <FormCheck id='underMaintenance' label='Under Maintenance' style={{color: '#FF9900'}}/>
+                    <FormCheck id='unavailable' label='Unavailable' style={{color: '#A3A3A3'}}/>
+                    <FormCheck id='removed' label='Removed' style={{color: '#E73838'}}/>
                 </Form>
             </Row>
             <Row className='mx-0 my-3 d-flex justify-content-center'>
                 <div className='hr'/>
             </Row>
 
-            <Row className='mx-0 d-flex justify-content-end px-1'>
+            <Row className='mx-0 d-flex justify-content-end px-1 mb-2'>
                 <div className='d-flex align-items-center justify-content-center filter-button px-2'>
                     <label>
                         <FiFilter color='white' size='16px'/>
