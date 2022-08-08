320ba4ed15bc7339f91390b4232816efc5d057fb
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-08 11:47:58 +0530

    Added handle filter submit

--- admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
+++ admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
@@ -3,13 +3,18 @@ import {Col, Form, FormCheck, Row} from "react-bootstrap";
 import {FiFilter} from "react-icons/fi";
 
 const GarbageHubsTableFilter = () => {
+
+    const handleOnFilterButtonClicked = () => {
+        console.log('filterrrr');
+    }
+
     return (
-        <div className=' position-absolute garbage-hubs-table-filter-wrapper h-50 me-3'>
+        <div className='position-absolute garbage-hubs-table-filter-wrapper h-50 me-3'>
             <Row className='mx-0'>
                 <Col lg={12}>Hub ID</Col>
                 <Col lg={12}>Bin Type</Col>
             </Row>
-            <Row className='mx-0'>
+            <Row className='mx-0 filters-section'>
                 <Form>
                     <FormCheck label='All'/>
                     <FormCheck label='Active'/>
@@ -18,12 +23,17 @@ const GarbageHubsTableFilter = () => {
                     <FormCheck label='Removed'/>
                 </Form>
             </Row>
+            <Row className='mx-0 my-3 d-flex justify-content-center'>
+                <div className='hr'/>
+            </Row>
+
             <Row className='mx-0 d-flex justify-content-end px-1'>
                 <div className='d-flex align-items-center justify-content-center filter-button px-2'>
                     <label>
                         <FiFilter color='white' size='16px'/>
                     </label>
                     <label className='ms-1'>Filter</label>
+                    <div className='filter-button-mask position-absolute' onClick={handleOnFilterButtonClicked}/>
                 </div>
             </Row>
         </div>
