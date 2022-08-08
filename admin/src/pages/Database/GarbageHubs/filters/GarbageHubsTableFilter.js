6139f608e99de722eab896796b7739f5b1efa4ef
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-08 11:32:39 +0530

    Completed filter button UI

--- admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
+++ admin/src/pages/Database/GarbageHubs/filters/GarbageHubsTableFilter.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Col, Form, FormCheck, Row} from "react-bootstrap";
+import {FiFilter} from "react-icons/fi";
 
 const GarbageHubsTableFilter = () => {
-  return (
-    <Container>
-      Garbage hubs table filter
-    </Container>
-  );
+    return (
+        <div className=' position-absolute garbage-hubs-table-filter-wrapper h-50 me-3'>
+            <Row className='mx-0'>
+                <Col lg={12}>Hub ID</Col>
+                <Col lg={12}>Bin Type</Col>
+            </Row>
+            <Row className='mx-0'>
+                <Form>
+                    <FormCheck label='All'/>
+                    <FormCheck label='Active'/>
+                    <FormCheck label='Under Maintenance'/>
+                    <FormCheck label='Unavailable'/>
+                    <FormCheck label='Removed'/>
+                </Form>
+            </Row>
+            <Row className='mx-0 d-flex justify-content-end px-1'>
+                <div className='d-flex align-items-center justify-content-center filter-button px-2'>
+                    <label>
+                        <FiFilter color='white' size='16px'/>
+                    </label>
+                    <label className='ms-1'>Filter</label>
+                </div>
+            </Row>
+        </div>
+    );
 }
 
 export default GarbageHubsTableFilter;
