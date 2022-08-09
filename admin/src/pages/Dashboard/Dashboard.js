31a71e88d1900bd6642f31ae8af76e4548e749b6
IndunJD <indunijd@gmail.com>
2022-08-09 20:51:53 +0530

    Dashboard-Garbage Collection Weight Graph

--- admin/src/pages/Dashboard/Dashboard.js
+++ admin/src/pages/Dashboard/Dashboard.js
@@ -20,19 +20,19 @@ const Dashboard = () => {
                 <Col className="p-0 mt-4" style={{
                     fontWeight: 800, fontSize: 30, color: '#075061'}} sm={12}>Hello Nisal,</Col>
                 <Col className="p-0 mb-3" sm={12}>This is what we have got for you today - {date}</Col>
-                <Col lg={10} className="p-0" style={{border: '2px solid red'}}>
+                <Col lg={10} className="p-0" style={{border: '2px solid #BFDDDE', borderRadius: '20px'}}>
                     <Row className="m-0" style={{border: '2px solid green'}}>
-                        <Col lg={12} className="p-0" style={{border: '2px solid pink'}}>Garbage Collection Weight</Col>
-                        <Col lg={12} className="p-0" style={{border: '2px solid pink'}}><GarbageCollectionWeightFilter/></Col>
-                        <Col lg={12} className="p-0" style={{border: '2px solid pink'}}>
-                            <Row className="m-0" style={{border: '2px solid yellow'}}>
-                                <Col lg={4} className="p-0" style={{border: '2px solid purple'}}><WeightCard/></Col>
-                                <Col lg={8} className="p-0" style={{border: '2px solid purple'}}>Chart</Col>
+                        <Col lg={12} className="p-0">Garbage Collection Weight</Col>
+                        <Col lg={12} className="p-0"><GarbageCollectionWeightFilter/></Col>
+                        <Col lg={12} className="p-0">
+                            <Row className="m-0">
+                                <Col lg={4} className="p-0"><WeightCard/></Col>
+                                <Col lg={8} className="p-0" style={{border: '2px solid purple'}}><GarbageCollectionWeightGraph/></Col>
                             </Row>
                         </Col>
                     </Row>
                 </Col>
-                <Col lg={2} className="p-0" style={{border: '2px solid red', display: "flex", flexDirection: "column"}}>
+                <Col lg={2} className="p-0" style={{display: "flex", flexDirection: "column"}}>
                     <Row className="m-0 ms-3"><CountCard/></Row>
                 </Col>
             </Row>
--- admin/src/pages/Dashboard/graphs/GarbageCollectionWeightGraph.js
+++ admin/src/pages/Dashboard/graphs/GarbageCollectionWeightGraph.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
+import {Line} from 'react-chartjs-2';
 
 const GarbageCollectionWeightGraph = () => {
-  return (
-    <Container>
-      Garbage collection weight graph
-    </Container>
-  );
+    return (
+        <Container className="p-0">
+            <Row className="m-0" style={{border: '2px solid red'}}>
+                <Col className="p-0" style={{border: '2px solid blue'}}>
+                    <Line
+                        type='line'
+                        data={{
+                            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                            datasets: [
+                                {
+                                    label: "Days",
+                                    data: [12, 19, 3, 5, 2, 3],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(253, 159, 64, 0.2)',
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(253, 159, 64, 0.2)',
+                                    ],
+                                },
+                            ],
+                        }}
+                        width={100}
+                        height={300}
+                        options={{
+                            maintainAspectRatio: false
+                        }}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default GarbageCollectionWeightGraph;
