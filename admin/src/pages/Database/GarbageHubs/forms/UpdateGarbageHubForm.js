0e667b8df6edf2a592ba3ee7b242204c791907b4
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-13 22:41:59 +0530

    Completed the update garbage hub form  model

--- admin/src/pages/Database/GarbageHubs/forms/UpdateGarbageHubForm.js
+++ admin/src/pages/Database/GarbageHubs/forms/UpdateGarbageHubForm.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 
 import GarbageHubCurrentLocationMap from "../maps/GarbageHubCurrentLocationMap";
 import GarbageHubNewLocationMap from "../maps/GarbageHubNewLocationMap";
 
 const UpdateGarbageHubForm = () => {
   return (
-    <Container>
-      Update garbage hub form
-    </Container>
+      <Container>
+          <Row>
+              <Col lg={12}>
+                  <div>Hub Id</div>
+                  <div>status</div>
+              </Col>
+              <Col lg={12} className='my-2'>
+                  <h3>Current Location</h3>
+                  <div>Map goes Here</div>
+              </Col>
+              <Col lg={12} className='my-2'>
+                  <h3>New Location</h3>
+                  <div>Latitude</div>
+                  <div>Longitude</div>
+                  <div>Map goes Here</div>
+              </Col>
+              <Col lg={12} className='my-2'>
+                  <Button>Save</Button>
+              </Col>
+          </Row>
+      </Container>
   );
 }
 
