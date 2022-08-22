dcbb6c41f9ac493d650c018729ec8b3adc9c3d2c
omaya98 <theruniomayawickramasinghe98@gmail.com>
2022-08-22 11:24:45 +0530

    created maintenance job page | assign job form/job map filter UI/UX

--- admin/src/pages/MaintenanceJobs/forms/AssignJobForm.js
+++ admin/src/pages/MaintenanceJobs/forms/AssignJobForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Container} from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import AssignJobMapFilter from "../filters/AssignJobMapFilter";
 import AssignJobMap from "../maps/AssignJobMap";
@@ -7,9 +7,251 @@ import AssignJobMap from "../maps/AssignJobMap";
 const AssignJobForm = () => {
   return (
     <Container>
-      Assign job form
+      <Row className="p-0 m-0 pb-3 ps-3">
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={6}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Crew Member
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <select
+              id="crewMember"
+              name="crewMember"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            >
+              <option value="">--Select--</option>
+              <option value="member1">Harith Kumar</option>
+            </select>
+          </Row>
+        </Col>
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={6}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Garbage Hub ID
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <select
+              id="hubId"
+              name="hubId"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            >
+              <option value="option1">CMB-07-12</option>
+              <option value="option2">CMB-07-13</option>
+            </select>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="p-0 m-0 ps-3 pb-2">
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={6}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Garbage Bin Type
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <select
+              id="binType"
+              name="binType"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            >
+              <option value="organic">Organic</option>
+              <option value="plastic">Plastic</option>
+              <option value="paper">Paper</option>
+              <option value="glass">Glass</option>
+            </select>
+          </Row>
+        </Col>
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={6}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Description
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <textarea
+              type="text"
+              rows="1"
+              cols="50"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            ></textarea>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="p-0 m-0 ps-3 pb-2">
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={4}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Fill Level
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <input
+              type="text"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            />
+          </Row>
+        </Col>
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={4}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Temperature
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <input
+              type="number"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            />
+          </Row>
+        </Col>
+        <Col
+          className={"m-0 p-0 pe-3"}
+          sm={4}
+          style={{
+            marginRight: 15,
+            marginLeft: 15,
+            fontWeight: "bold",
+            paddingBottom: 5,
+          }}
+        >
+          <Row className={"m-0 p-0 pb-2"} style={{ paddingBottom: 5 }}>
+            Humidity
+          </Row>
+          <Row className={"m-0 p-0 "}>
+            <input
+              type="text"
+              style={{
+                outline: "none",
+                border: "2px solid #E8F5F6",
+                borderRadius: 8,
+                paddingBottom: 3,
+                paddingTop: 3,
+                fontSize: 12,
+              }}
+            />
+          </Row>
+        </Col>
+      </Row>
+      <Row className="p-0 m-0 ps-3 pb-2 pt-2">
+        <Col sm={4} className="p-0 m-0" style={{ fontWeight: "bold" }}>
+          Location
+        </Col>
+        <Col sm={5}></Col>
+        <Col sm={3} className="p-0 pe-2 m-0">
+          <AssignJobMapFilter />
+        </Col>
+      </Row>
+      <Row className="p-3 pt-1 m-0" style={{ height: 200 }}>
+        <Col
+          className="p-0 m-0"
+          style={{ borderRadius: 10, border: "3px solid #E8F5F6" }}
+        >
+          <AssignJobMap />
+        </Col>
+      </Row>
+      <Row className="m-0 p-3 pt-0">
+        <Col className="p-0 m-0" sm={10}></Col>
+        <Col className="p-0 m-0" sm={2}>
+          <Button
+            className=" p-2"
+            style={{
+              borderRadius: "10px",
+              boxShadow: `5px 6px 8px #9999ff`,
+              width: "100%",
+            }}
+          >
+            Assign
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
-}
+};
 
 export default AssignJobForm;
