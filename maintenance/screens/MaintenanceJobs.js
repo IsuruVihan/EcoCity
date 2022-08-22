ef6751eb6e7af150b001eaabc59d1a554f6a8540
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-08-22 10:03:27 +0530

    On the way

---
+++ maintenance/screens/MaintenanceJobs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from "react-native";
+
+const MaintenanceJobs = () => {
+  return (
+    <Text>
+      Maintenance Jobs
+    </Text>
+  );
+}
+export default MaintenanceJobs;
