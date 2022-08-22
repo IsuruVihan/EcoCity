b7f1fffc9de4f8491282d69dc949fe87016f26b9
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-08-22 12:45:33 +0530

    Status part done

--- citizen/screens/MyProfile.js
+++ citizen/screens/MyProfile.js
@@ -1,10 +1,14 @@
 import React, {useContext} from 'react';
-import {Dimensions, StyleSheet, Text, View} from "react-native";
+import {Dimensions, Image, StyleSheet, Text, View} from "react-native";
 import Spinner from "react-native-loading-spinner-overlay/src";
 import {Button} from "@rneui/base";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Octicons from "react-native-vector-icons/Octicons";
 
 import {AuthContext} from "../context/AuthContext";
 import {Responsive} from "../helpers/Responsive";
+
+import HouseImg from '../assets/images/house-profile.png';
 
 const HEIGHT = Dimensions.get('window').height;
 
@@ -18,7 +22,27 @@ const MyProfile = () => {
         <Text style={styles.myProfile.topic.txt}>My Profile</Text>
       </View>
       <View style={styles.myProfile.space}/>
-      <View style={styles.myProfile.status}></View>
+      <View style={styles.myProfile.status}>
+        <View style={styles.myProfile.status.first}>
+          <Image source={HouseImg} style={styles.myProfile.status.first.img}/>
+        </View>
+        <View style={styles.myProfile.status.second}>
+          <Text style={styles.myProfile.status.second.txt1}>Welcome</Text>
+          <View style={styles.myProfile.status.second.container}>
+            <Text style={styles.myProfile.status.second.container.name}>Isuru Harischandra</Text>
+            <Octicons
+              name={'dot-fill'}
+              size={25}
+              color={'#00C186'}
+              style={styles.myProfile.status.second.container.icon}
+            />
+          </View>
+          <Text style={styles.myProfile.status.second.txt2}>Citizen ID: 1234</Text>
+        </View>
+        <View style={styles.myProfile.status.third}>
+          <MaterialIcons name={'logout'} size={25} color={'#075061'}/>
+        </View>
+      </View>
       <View style={styles.myProfile.links}></View>
       {/*<Spinner visible={loading}/>*/}
       {/*<View>*/}
@@ -36,8 +60,8 @@ const styles = StyleSheet.create({
     height: Responsive(83, HEIGHT),
     paddingHorizontal: 10,
     space: {
-      borderWidth: 2,
-      borderColor: 'red',
+      // borderWidth: 2,
+      // borderColor: 'red',
       height: '2%',
     },
     topic: {
@@ -51,13 +75,69 @@ const styles = StyleSheet.create({
         borderColor: 'green',
         color: '#042434',
         fontSize: 30,
-        fontWeight: '700',
+        fontWeight: '600',
       },
     },
     status: {
-      borderWidth: 2,
-      borderColor: 'red',
+      borderTopWidth: 2,
+      borderTopColor: '#BFDDDE',
+      borderBottomWidth: 2,
+      borderBottomColor: '#BFDDDE',
       height: '20%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      first: {
+        // borderWidth: 2,
+        // borderColor: 'blue',
+        width: '30%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        img: {
+          height: '60%',
+          width: '60%',
+        },
+      },
+      second: {
+        // borderWidth: 2,
+        // borderColor: 'blue',
+        width: '60%',
+        display: 'flex',
+        justifyContent: 'center',
+        txt1: {
+          color: '#949494',
+          fontSize: 15,
+        },
+        container: {
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          name: {
+            color: '#042434',
+            fontSize: 20,
+            fontWeight: '500',
+            // borderWidth: 2,
+            // borderColor: 'blue',
+          },
+          icon: {
+            marginLeft: 3,
+            // borderWidth: 2,
+            // borderColor: 'blue',
+          },
+        },
+        txt2: {
+          color: '#042434',
+          fontSize: 12,
+        },
+      },
+      third: {
+        // borderWidth: 2,
+        // borderColor: 'blue',
+        width: '10%',
+        display: 'flex',
+        justifyContent: 'center',
+      },
     },
     links: {
       borderWidth: 2,
