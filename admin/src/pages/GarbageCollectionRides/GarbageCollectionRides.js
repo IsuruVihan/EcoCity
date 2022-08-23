4006e6c1730d655a45ed8b9726775290d3e3cb77
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-24 00:45:58 +0530

    Alignments in table filter

--- admin/src/pages/GarbageCollectionRides/GarbageCollectionRides.js
+++ admin/src/pages/GarbageCollectionRides/GarbageCollectionRides.js
@@ -8,49 +8,64 @@ import ViewRideModal from "./modals/ViewRideModal";
 import AssignRideModal from "./modals/AssignRideModal";
 import Header from "../../common/Header";
 import filtericon from "../../assets/images/icons/filter.png";
+import {FiFilter} from "react-icons/fi";
 
 const GarbageCollectionRides = () => {
     const [open, setOpen] = useState(false);
-
-  return (
-      <Col lg={10}>
-          <Row className="p-0 m-0 mt-4 ps-3">
-              <Col className=" p-0 m-0" style={{textAlign: 'left', fontSize: 30, fontWeight: '900', color: '#042434',}}>
-                  Garbage Collection Rides
-              </Col>
-          </Row>
-          <Row>
-              <Col style={{border:"1px solid #BFDDDE" , borderRadius:10}} className="m-2">
-                  <Row className="d-flex">
-                      <Col xs={10}></Col>
-                      <Col className='pt-3 mx-3 me-2'>
-                          <Row xs="2" className="mx-2" style={{border:'2px solid #BFDDDE', borderRadius:10 , textAlign:'right',}}
-                               onClick={() => setOpen(!open)} >
-                              <Col style={{cursor:'pointer',}} aria-controls="example-collapse-text" aria-expanded={open} xs="6">
-                                  Filter
-                              </Col>
-                              <Col style={{textAlign:'right',cursor:'pointer'}}>
-                                  <Image src={filtericon} width="50%"></Image>
-                              </Col>
-                          </Row>
-                      </Col>
-                  </Row>
-                  <Row style={{position: 'absolute', right: 25, paddingTop: 8, zIndex:2}}>
-                      <Collapse in={open}>
-                          <Container id="example-collapse-text" style={{backgroundColor:'#fff'}}>
-                              <RidesTableFilter/>
-                          </Container>
-                      </Collapse>
-                  </Row>
-                  <Row>
-                      <Col className="m-4">
-                          <RidesTable/>
-                      </Col>
-                  </Row>
-              </Col>
-          </Row>
-      </Col>
-  );
+    return (
+        <Col lg={10}>
+            <Row className="p-0 m-0 mt-4 ps-3">
+                <Col className=" p-0 m-0"
+                     style={{textAlign: 'left', fontSize: 30, fontWeight: '900', color: '#042434',}}>
+                    Garbage Collection Rides
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{border: "1px solid #BFDDDE", borderRadius: 10}} className="m-2">
+                    <Row className="d-flex">
+                        {/*<Col xs={10}></Col>*/}
+                        {/*<Col className='pt-3 mx-3 me-2'>*/}
+                        {/*    <Row xs="2" className="mx-2"*/}
+                        {/*         style={{border: '2px solid #BFDDDE', borderRadius: 10, textAlign: 'right',}}*/}
+                        {/*         onClick={() => setOpen(!open)}>*/}
+                        {/*        <Col style={{cursor: 'pointer',}} aria-controls="example-collapse-text"*/}
+                        {/*             aria-expanded={open} xs="6">*/}
+                        {/*            Filter*/}
+                        {/*        </Col>*/}
+                        {/*        <Col style={{textAlign: 'right', cursor: 'pointer'}}>*/}
+                        {/*            <Image src={filtericon} width="50%"></Image>*/}
+                        {/*        </Col>*/}
+                        {/*    </Row>*/}
+                        {/*</Col>*/}
+                        <Row className="d-flex">
+                            <Col>
+                                <div className='mt-2 d-flex justify-content-end'>
+                        <span className='filter-box px-2 me-1' onClick={() => {
+                            setOpen(!open)
+                        }}>
+                            Filter
+                            <FiFilter color='#228693' size='20px'/>
+                        </span>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Row>
+                    <Row style={{position: 'absolute', right: 25, paddingTop: 8, zIndex: 2}}>
+                        <Collapse in={open}>
+                            <Container id="example-collapse-text" style={{backgroundColor: '#fff'}}>
+                                <RidesTableFilter/>
+                            </Container>
+                        </Collapse>
+                    </Row>
+                    <Row>
+                        <Col className="m-4">
+                            <RidesTable/>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Col>
+    );
 }
 
 export default GarbageCollectionRides;
