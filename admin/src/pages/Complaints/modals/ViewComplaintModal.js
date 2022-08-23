06995959e9b5cb03bcef8d5eb2d5dd3715b6a5a7
IndunJD <indunijd@gmail.com>
2022-08-23 23:29:11 +0530

    Complaints-Complaint popup model UI/UX fixed

--- admin/src/pages/Complaints/modals/ViewComplaintModal.js
+++ admin/src/pages/Complaints/modals/ViewComplaintModal.js
@@ -13,12 +13,13 @@ const ViewComplaintModal = () => {
                 <Image src={modelimage} fluid style={{width:'25%'}}/>
             </Col>
         </Row>
-        <Row style={{border:'1px solid green'}}>
+        <Row>
             <Col>
-                <Row style={{border:'1px solid green', textAlign:'center'}} className="d-flex">
+                <Row style={{textAlign:'center'}} className="d-flex">
                     <Col
-                        style={{marginRight:130, marginLeft:130,marginTop:8, backgroundColor:'#E1F0FF', borderRadius:10, color:'#008BD1', padding:5, fontWeight:'bold'}}>
-                        Viewed Complaints
+                        style={{marginRight:130, marginLeft:130,marginTop:8, backgroundColor:'#E1F0FF',
+                                borderRadius:10, color:'#008BD1', padding:5, fontWeight:'bold'}}>
+                        Viewed Complaint
                     </Col>
                 </Row>
                 <Row style={{marginTop:10}}>
