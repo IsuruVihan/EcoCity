cab5af22fa7c914d3d122dbeba9cd17cb3edc1fe
HarshaAbeyvickrama <harshaabeyvickrama@gmail.com>
2022-08-23 23:38:52 +0530

    Changes made to view modal

--- admin/src/pages/Complaints/forms/RemarksForm.js
+++ admin/src/pages/Complaints/forms/RemarksForm.js
@@ -8,14 +8,26 @@ const RemarksForm = () => {
     <Container>
         <Row>
             <Col >
-                <Row style={{border:'1px solid red', }}>
-                    <Col style={{height:300}} className="overflow-auto">
-                        <Row >
-                            <Col style={{marginRight:15 , marginLeft:15 , fontSize:13, fontWeight:'bold', paddingBottom:5}}>
-                                <Row style={{paddingBottom:5}}>Complaint ID</Row>
+                <Row>
+                    <Col style={{height: 300}} className="overflow-auto">
+                        <Row>
+                            <Col style={{
+                                marginRight: 15,
+                                marginLeft: 15,
+                                fontSize: 13,
+                                fontWeight: 'bold',
+                                paddingBottom: 5
+                            }}>
+                                <Row style={{paddingBottom: 5}}>Complaint ID</Row>
                                 <Row>
                                     <input type="text"
-                                           style={{outline:'none', border:'1px solid #E8F5F6', borderRadius:8, paddingBottom:5, fontSize:12}}/>
+                                           style={{
+                                               outline: 'none',
+                                               border: '1px solid #E8F5F6',
+                                               borderRadius: 8,
+                                               paddingBottom: 5,
+                                               fontSize: 12
+                                           }}/>
                                 </Row>
                             </Col>
                         </Row>
@@ -91,13 +103,22 @@ const RemarksForm = () => {
                 </Row>
                 <Row>
                     <Col
-                        style={{border:'1px solid green', marginLeft:110, marginRight:110, display:'flex', justifyContent:'space-evenly'}}>
+                        style={{marginLeft: 110, marginRight: 110, display: 'flex', justifyContent: 'space-evenly'}}>
                         <button
-                            style={{width:100 ,fontSize:15, padding:5, borderRadius:8, border:'none'}}>
+                            style={{width: 100, fontSize: 15, padding: 5, borderRadius: 8, border: 'none'}}>
                             Reject
                         </button>
                         <button
-                            style={{width:100 ,fontSize:15, padding:5, borderRadius:8, border:'none', backgroundColor:'#228693', color:'#fff', fontWeight:'bold'}}>
+                            style={{
+                                width: 100,
+                                fontSize: 15,
+                                padding: 5,
+                                borderRadius: 8,
+                                border: 'none',
+                                backgroundColor: '#228693',
+                                color: '#fff',
+                                fontWeight: 'bold'
+                            }}>
                             Resolve
                         </button>
                     </Col>
