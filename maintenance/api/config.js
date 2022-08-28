641849d4b491d802dbe20163cac77e1b8585fba3
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-08-28 22:33:13 +0530

    Title ok

--- maintenance/api/config.js
+++ maintenance/api/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://192.168.1.227:3001/api';
-export const BASE_URL = 'http://10.22.167.132:3001/api';
+export const BASE_URL = 'http://192.168.1.227:3001/api';
+// export const BASE_URL = 'http://10.22.167.132:3001/api';
--- maintenance/screens/MaintenanceJobs.js
+++ maintenance/screens/MaintenanceJobs.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import {Text} from "react-native";
+import {Text, View, StyleSheet} from "react-native";
 
 const MaintenanceJobs = () => {
   return (
-    <Text>
-      Maintenance Jobs
-    </Text>
+    <View style={styles.maintenance}>
+      <View style={styles.maintenance.one}>
+        <Text style={styles.maintenance.one.txt}>Maintenance Jobs</Text>
+      </View>
+      <View style={styles.maintenance.two}></View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  maintenance: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    one: {
+      borderWidth: 2,
+      borderColor: 'orange',
+      flex: 1,
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      txt: {
+        color: '#042434',
+        fontSize: 25,
+        fontWeight: '600',
+      },
+    },
+    two: {
+      borderWidth: 2,
+      borderColor: 'orange',
+      flex: 11,
+      width: '100%',
+    },
+  },
+});
+
 export default MaintenanceJobs;
