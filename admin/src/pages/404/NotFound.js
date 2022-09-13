50b595957e6241df1c340a26b9ec941e8a817648
IndunJD <indunijd@gmail.com>
2022-09-13 10:37:30 +0530

    404-created a Container

--- admin/src/pages/404/NotFound.js
+++ admin/src/pages/404/NotFound.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
 import {Container, Row, Col, Image} from "react-bootstrap";
+
+const NotFoundModal = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+}
