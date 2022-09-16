ff4265c7009d6c0267dada8e74b6ce2fffa1ec2a
IndunJD <indunijd@gmail.com>
2022-09-16 23:12:31 +0530

    404-Modified NotFound.js

--- admin/src/pages/404/NotFound.js
+++ admin/src/pages/404/NotFound.js
@@ -5,7 +5,7 @@ import {Container, Row, Col, Image} from "react-bootstrap";
 const NotFoundModal = () => {
     return (
         <Container>
-            ABCDEFGHIJKLMNOPQRS
+            ABCDEFGHIJKLMNOPQRST
         </Container>
     );
 }
