3efbe394e2478d7cb6636deaf51a237c2401ec49
IndunJD <indunijd@gmail.com>
2022-09-23 18:52:49 +0530

    404-Modified NotFound.js

--- admin/src/pages/404/NotFound.js
+++ admin/src/pages/404/NotFound.js
@@ -39,6 +39,23 @@ const NotFoundModal = () => {
                 18
                 19
                 20
+                ABCDEFGHIJKLMNOPQRSTUVWXYZ
+                1
+                2
+                3
+                4
+                5
+                6
+                7
+                8
+                9
+                10
+                11 12 13 14 15
+                16
+                17
+                18
+                19
+                20
         </Container>
     );
 }
