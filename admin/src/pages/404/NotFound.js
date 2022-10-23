2321c6005f441e1deb2e536cde4e6e9b1805ce39
IndunJD <indunijd@gmail.com>
2022-10-23 10:03:06 +0530

    404-Modified NotFound.js

--- admin/src/pages/404/NotFound.js
+++ admin/src/pages/404/NotFound.js
@@ -29,6 +29,25 @@ const NotFoundModal = () => {
     }
 }
 The same text expressed as XML:
+<!DOCTYPE glossary PUBLIC "-//OASIS//DTD DocBook V3.1//EN">
+ <glossary><title>example glossary</title>
+  <GlossDiv><title>S</title>
+   <GlossList>
+    <GlossEntry ID="SGML" SortAs="SGML">
+     <GlossTerm>Standard Generalized Markup Language</GlossTerm>
+     <Acronym>SGML</Acronym>
+     <Abbrev>ISO 8879:1986</Abbrev>
+     <GlossDef>
+      <para>A meta-markup language, used to create markup
+languages such as DocBook.</para>
+      <GlossSeeAlso OtherTerm="GML">
+      <GlossSeeAlso OtherTerm="XML">
+     </GlossDef>
+     <GlossSee OtherTerm="markup">
+    </GlossEntry>
+   </GlossList>
+  </GlossDiv>
+ </glossary>
         */}
         </Container>
     );
