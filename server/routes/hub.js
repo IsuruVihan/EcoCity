e4900fc2a34c1310af03bc0c5d91dcb1ec63e14c
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-10-25 08:49:07 +0530

    Done

--- server/routes/hub.js
+++ server/routes/hub.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // Controllers
-const {openLid} = require('../controllers/hub');
+const {openLid, dumpGarbage} = require('../controllers/hub');
 
 // Endpoints
 router.post('/hub/open', openLid);
+router.post('/hub/dump', dumpGarbage);
 // router.put('/complaints', validateToken, removeComplaint);
 // router.get('/complaints/all', validateToken, getAllComplaints);
 // router.post('/complaints/update', validateToken, resolveReject);
